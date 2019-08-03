38b5961253188eefaf558ba6060220ba7d7abe85
jinkoooooo <52557230+jinkoooooo@users.noreply.github.com>
2019-08-03 16:33:02 +0900

    food UP

---
+++ FoodChart/FoodChart/src/FoodChart/DB_MAN.java
@@ -0,0 +1,45 @@
+package FoodChart;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//import .*;
+import java.sql.*;
+import java.io.*;
+import java.util.*;
+/**
+ *
+ * @author Administrator
+ */
+public class DB_MAN {
+    String strDriver = "com.mysql.jdbc.Driver";
+    String strURL = "jdbc:mysql://localhost:3306/membera";
+    String strUser = "root";
+    String strPass = "djswpsk0303!@";
+    
+    Connection DB_con;
+    Statement DB_stmt;
+    ResultSet DB_rs;
+    
+    public void dbOpen() throws IOException{
+        try{
+            Class.forName(strDriver);
+            DB_con = DriverManager.getConnection(strURL, strUser, strPass);
+            DB_stmt = DB_con.createStatement();
+            
+        }catch (Exception e){
+            System.out.println("SQLException:"+e.getMessage());
+        }
+    }
+    public void dbClose() throws IOException{
+        try{
+            DB_stmt.close();
+            DB_con.close();
+        }catch(Exception e){
+            System.out.println("SQLException:"+e.getMessage());
+        }
+    }
+    
+}
